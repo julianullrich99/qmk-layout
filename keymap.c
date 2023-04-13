f0d094ff3929aa7a537db9c8e8b5ca99f4a8fa9b
Julian Ullrich <julian.ullrich@falcondev.de>
2023-04-13 18:30:08 +0200

    add neo and gaming layer

--- keymap.c
+++ keymap.c
@@ -1,5 +1,16 @@
 #include QMK_KEYBOARD_H
 #include "rgb_matrix.h"
+#include "keymap_german.h"
+
+enum layer_names {
+  _BASE,
+  _NUMBERS,
+  _CONTROL,
+  _RGB,
+  _NEO,
+  _NEO_EXTRA,
+  _LEAGUE
+};
 
 /* THIS FILE WAS GENERATED!
  *
@@ -32,40 +43,62 @@ const uint8_t PROGMEM ledmap[][DRIVER_LED_TOTAL][4] = {{{0, 0, 0}, {0, 0, 0}, {0
                                                                    {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}},
                                                                    {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}}};
 
-void set_layer_color(int layer) {
-  rgb_matrix_enable_noeeprom();
-  rgb_matrix_mode_noeeprom(RGB_MATRIX_NONE);
-  for (int i = 0; i < DRIVER_LED_TOTAL; i++) {
-    int r = pgm_read_byte(&ledmap[layer][i][0]);
-    int g = pgm_read_byte(&ledmap[layer][i][1]);
-    int b = pgm_read_byte(&ledmap[layer][i][2]);
-    rgb_matrix_set_color(i, r, g, b);
-  }
-}
+// void set_layer_color(int layer) {
+//   rgb_matrix_enable_noeeprom();
+//   rgb_matrix_mode_noeeprom(RGB_MATRIX_NONE);
+//   for (int i = 0; i < DRIVER_LED_TOTAL; i++) {
+//     int r = pgm_read_byte(&ledmap[layer][i][0]);
+//     int g = pgm_read_byte(&ledmap[layer][i][1]);
+//     int b = pgm_read_byte(&ledmap[layer][i][2]);
+//     rgb_matrix_set_color(i, r, g, b);
+//   }
+// }
 
-void keyboard_post_init_user(void) {
-    rgb_matrix_enable();
-    rgb_matrix_mode(RGB_MATRIX_CYCLE_LEFT_RIGHT);
-    // rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
-    // rgb_matrix_sethsv_noeeprom(HSV_OFF);
-    // rgb_matrix_mode(RGB_MATRIX_SOLID_REACTIVE_SIMPLE);
-    // set_layer_color(0);
-    // rgb_matrix_set_color_all(200, 100, 200);
-    // const uint8_t layer = 0;
-    // for (int i = 0; i < DRIVER_LED_TOTAL; i++) {
-    //   int r = pgm_read_byte(&ledmap[layer][i][0]);
-    //   int g = pgm_read_byte(&ledmap[layer][i][1]);
-    //   int b = pgm_read_byte(&ledmap[layer][i][2]);
-    //   rgb_matrix_set_color(i, r, g, b);
-    // }
-}
+// void keyboard_post_init_user(void) {
+//     rgb_matrix_enable();
+//     rgb_matrix_mode(RGB_MATRIX_CYCLE_LEFT_RIGHT);
+//     // rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
+//     // rgb_matrix_sethsv_noeeprom(HSV_OFF);
+//     // rgb_matrix_mode(RGB_MATRIX_SOLID_REACTIVE_SIMPLE);
+//     // set_layer_color(0);
+//     // rgb_matrix_set_color_all(200, 100, 200);
+//     // const uint8_t layer = 0;
+//     // for (int i = 0; i < DRIVER_LED_TOTAL; i++) {
+//     //   int r = pgm_read_byte(&ledmap[layer][i][0]);
+//     //   int g = pgm_read_byte(&ledmap[layer][i][1]);
+//     //   int b = pgm_read_byte(&ledmap[layer][i][2]);
+//     //   rgb_matrix_set_color(i, r, g, b);
+//     // }
+// }
 
+bool neo_on = false;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case LT(0,KC_SCLN):
             if (!record->tap.count && record->event.pressed) {
                 tap_code16(KC_QUOT); // Intercept hold function
+                return false;
+            }
+            return true;
+
+        case LT(1,KC_NUBS):
+            if (!record->tap.count && record->event.pressed) {
+                tap_code16(KC_Z); // Intercept hold function
+                return false;
+            }
+            return true;
+
+        case LT(1,KC_D):
+            if (!record->tap.count && record->event.pressed) {
+                tap_code16(DE_Y); // Intercept hold function
+                return false;
+            }
+            return true;
+
+        case LT(1,DE_COLN):
+            if (!record->tap.count && record->event.pressed) {
+                tap_code16(DE_AT); // Intercept hold function
                 return false;
             }
             return true;
@@ -79,37 +112,107 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             return false;
           }
           return true;
+
+        case TO(_NEO):
+          if (!record->event.pressed) return false; // dont do anything on lift button
+          if (neo_on) { //toggle neo layer
+            layer_off(_NEO);
+            rgb_matrix_mode_noeeprom(RGB_MATRIX_CYCLE_LEFT_RIGHT);
+            neo_on = false;
+          } else {
+            layer_on(_NEO);
+            rgb_matrix_mode_noeeprom(RGB_MATRIX_NONE);
+            rgb_matrix_sethsv_noeeprom(HSV_GREEN);
+            neo_on = true;
+          }
+          return false;
+      
+        case RSFT_T(KC_SPC): // magic key for toggling numbers from neo layer
+            if (!record->tap.count && record->event.pressed) {
+                layer_off(_NEO);
+                layer_on(_NUMBERS);
+                return false;
+            } else if (!record->tap.count) {
+              if (neo_on == true) layer_on(_NEO);
+              layer_off(_NUMBERS);
+              return false;
+            }
+            return true;
+
+        case RSFT_T(KC_BSPC): // magic key for toggling controls from neo layer
+            if (!record->tap.count && record->event.pressed) {
+                layer_off(_NEO);
+                layer_on(_CONTROL);
+                return false;
+            } else if (!record->tap.count) {
+              if (neo_on == true) layer_on(_NEO);
+              layer_off(_CONTROL);
+              return false;
+            }
+            return true;
+
+        case TO(_LEAGUE):
+          if (record->event.pressed) {
+            rgb_matrix_mode_noeeprom(RGB_MATRIX_CYCLE_ALL);
+          }
+          return true;
+
+        case TO(_BASE):
+          if (record->event.pressed) {
+            rgb_matrix_mode_noeeprom(RGB_MATRIX_CYCLE_LEFT_RIGHT);
+          }
+          return true;
     }
     return true;
 }
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_split_3x5_2(
+    [_BASE] = LAYOUT_split_3x5_2(
       KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P,
       KC_A, LALT_T(KC_S), LCTL_T(KC_D), KC_F, KC_G, KC_H, KC_J, LCTL_T(KC_K), LALT_T(KC_L), LT(0,KC_SCLN),
       LGUI_T(KC_Z), KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,
 
-      LT(2, KC_BSPC), KC_LSFT, LT(1, KC_SPC), RSFT_T(KC_ENT)
+      LT(_CONTROL, KC_BSPC), KC_LSFT, LT(_NUMBERS, KC_SPC), RSFT_T(KC_ENT)
       ),
-    [1] = LAYOUT_split_3x5_2(
+    [_NUMBERS] = LAYOUT_split_3x5_2(
       KC_GRV, KC_NO, KC_PAGE_UP, KC_PAGE_DOWN, KC_NO, KC_LCTL, KC_7, KC_8, KC_9, KC_LBRC,
       KC_NO, KC_NO, KC_PPLS, KC_PMNS, KC_NO, KC_COMM, KC_4, KC_5, KC_6, KC_RBRC,
-      QK_BOOTLOADER, TO(3), KC_PAST, KC_PSLS, KC_NO, KC_DOT, KC_1, KC_2, KC_3, KC_NO,
+      TO(_LEAGUE), TO(_NEO), KC_PAST, KC_PSLS, KC_NO, KC_DOT, KC_1, KC_2, KC_3, KC_NO,
 
-      KC_BSPC, KC_LSFT, KC_NO, KC_0
+      KC_BSPC, KC_LSFT, RSFT_T(KC_SPC), KC_0 //have the magic key here to go back to neo if needed. normaly not called at all bc of LT in _BASE
       ),
-    [2] = LAYOUT_split_3x5_2(
+    [_CONTROL] = LAYOUT_split_3x5_2(
       KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,
       KC_F11, KC_LALT, KC_LCTL, KC_ESC, KC_TAB, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_F12,
       KC_MUTE, KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_NEXT_TRACK, KC_HOME, KC_TAB, KC_DEL, KC_END, KC_PRINT_SCREEN,
 
-      KC_NO, KC_NO, KC_LSFT, KC_ENT
+      KC_NO, TO(_RGB), KC_LSFT, KC_ENT
       ),
-    [3] = LAYOUT_split_3x5_2(
+    [_RGB] = LAYOUT_split_3x5_2(
       KC_NO, KC_NO, KC_NO, RGB_TOG, KC_NO, RGB_VAI, RGB_SPD, RGB_SPI, KC_NO, KC_NO,
       KC_NO, KC_NO, RGB_MODE_REVERSE, RGB_MODE_FORWARD, KC_NO, RGB_VAD, RGB_HUD, RGB_HUI, KC_NO, KC_NO,
-      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, RGB_SAD, RGB_SAI, KC_NO, KC_NO,
-      KC_NO, TO(0), KC_LSFT, KC_NO
-    )
+      QK_BOOTLOADER, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, RGB_SAD, RGB_SAI, KC_NO, KC_NO,
+      KC_NO, TO(_BASE), KC_LSFT, KC_NO
+    ),
+    [_NEO] = LAYOUT_split_3x5_2(
+      KC_X, KC_V, KC_L, KC_C, KC_W, KC_K, KC_H, KC_G, KC_F, KC_Q,
+      KC_U, LALT_T(KC_I), LCTL_T(KC_A), KC_E, KC_O, KC_S, KC_N, LCTL_T(KC_R), LALT_T(KC_T), LT(1, KC_D),
+      LGUI_T(DE_UDIA), DE_ODIA, DE_ADIA, KC_P, DE_Z, KC_B, KC_M, KC_COMM, KC_DOT, KC_J,
+
+      RSFT_T(KC_BSPC), KC_LSFT, RSFT_T(KC_SPC), LT(_NEO_EXTRA, KC_ENT)),
+
+    [_NEO_EXTRA] = LAYOUT_split_3x5_2(
+      DE_AT, S(DE_MINS), DE_LBRC, DE_RBRC, KC_GRV, DE_EXLM, DE_LABK, DE_RABK, DE_EQL, DE_AMPR,
+      DE_BSLS, DE_SLSH, DE_LCBR, DE_RCBR, DE_ASTR, DE_QUES, DE_LPRN, DE_RPRN, DE_MINS, LT(1, DE_COLN),
+      DE_HASH, DE_DLR, DE_PIPE, DE_TILD, DE_GRV, DE_PLUS, DE_PERC, DE_DQUO, DE_QUOT, DE_SCLN,
+
+      RSFT_T(KC_BSPC), KC_LSFT, RSFT_T(KC_SPC), RSFT_T(KC_ENT)),
+    [_LEAGUE] = LAYOUT_split_3x5_2(
+      KC_TAB, KC_2, KC_3, KC_4, KC_5,   KC_Y, KC_U, KC_I, KC_O, KC_P,
+      KC_Q, KC_W, KC_E, KC_R, KC_T,   KC_H, KC_J, LCTL_T(KC_K), LALT_T(KC_L), LT(0,KC_SCLN),
+      LT(1, KC_NUBS), KC_S, KC_LCTL, KC_D, KC_F,  KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,
+
+      KC_LALT, KC_SPC, LT(_NUMBERS, KC_SPC), TO(_BASE)
+      ),
 };
